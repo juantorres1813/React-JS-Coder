581d7c346ade1ca2417482c19319a63a3edf799a
Juan Torres <juantorres1813@gmail.com>
2022-08-04 04:54:30 -0300

    desafio intermedio

--- src/components/NavBar/Navbar.js
+++ src/components/NavBar/Navbar.js
@@ -11,7 +11,7 @@ const NavBar = () => {
         <Navbar bg="dark" variant={"dark"} expand="lg">
             <Container>
             <Navbar.Brand>
-            <Link to="/"><img alt="" src="/assets/multimedia/logo.jpg" width="200" height="100" className="d-inline-block align-top"/></Link>
+            <Link to="/"><img alt="" src="/assets/multimedia/logo.jpg"className="d-inline-block align-top navimg"/></Link>
             </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
@@ -19,7 +19,9 @@ const NavBar = () => {
                         <ul className='ul-menu'>
                             <Link to="/"><li><button>Home</button></li></Link>
                             <Link to="/catalogo"><li><button>Catálogo</button></li></Link>
-                            <Link to="/categorias/buzosysweaters"><li><button>Buzos y Sweaters</button></li></Link>
+                            <Link to="/categorias/bolsosybandoleras"><li><button>Bolsos y Bandoleras</button></li></Link>
+                            <Link to="/categorias/cartucheras"><li><button>Cartucheras</button></li></Link>
+                            <Link to="/categorias/portacelulares"><li><button>Portacelulares</button></li></Link>
                         </ul>
                     </Nav>
                 </Navbar.Collapse>
--- src/utils/details.mock.js
+++ src/utils/details.mock.js
@@ -1,46 +1,68 @@
 const detailmock = [{
     id:1,
-    title: "Campera",
-    price: 9000,
-    image: 'logo.jpg',
-    image2: 'logo.jpg',
-    image3: 'logo.jpg',
-    description: "Campera Azul Mistral Holden",
+    title: "Bolso Matero",
+    price: 3900,
+    image: 'producto1.jpg',
+    image2: 'producto1.jpg',
+    image3: 'producto1.jpg',
+    description: "Bolsos Materos con Lona",
     stock: 6,
-    category: "camperas"
+    category: "bolsosybandoleras"
 },
 {
     id:2,
-    title: "Remera",
-    price: 2500,
-    image: 'logo.jpg',
+    title: "Cartuchera",
+    price: 450,
+    image: 'producto2.jpeg',
     stock: 5,
-    image2: 'logo.jpg',
-    image3: 'logo.jpg',
-    description: "Remera Verde Mistral",
-    category: "remeras"
+    image2: 'producto2.jpeg',
+    image3: 'producto2.jpeg',
+    description: "Cartuchera Triangular",
+    category: "cartucheras"
 },
 {
     id:3,
-    title: "Buzo",
-    price: 1800,
-    image: 'logo.jpg',
+    title: "Porta Celular",
+    price: 450,
+    image: 'producto3.jpg',
     stock: 4,
-    image2: 'logo.jpg',
-    image3: 'logo.jpg',
-    description: "Buzo Azul Mistral Holden",
-    category: "buzosysweaters"
+    image2: 'producto3.jpg',
+    image3: 'producto3.jpg',
+    description: "Porta Celular de colores",
+    category: "portacelulares"
 },
 {
     id:4,
-    title: "Sweater",
-    price: 1500,
-    image: 'logo.jpg',
+    title: "Cartuchera",
+    price: 1800,
+    image: 'producto4.jpeg',
     stock: 5,
-    image2: 'logo.jpg',
-    image3: 'logo.jpg',
-    description: "Sweater Azul Mistral",
-    category: "buzosysweaters"
+    image2: 'producto4.jpeg',
+    image3: 'producto4.jpeg',
+    description: "Cartuchera Ipanema",
+    category: "cartucheras"
+},
+{
+    id:5,
+    title: "Bolsa",
+    price: 1000,
+    image: 'producto5.jpeg',
+    stock: 5,
+    image2: 'producto5.jpeg',
+    image3: 'producto5.jpeg',
+    description: "Bolsa de Arte",
+    category: "bolsosybandoleras"
+},
+{
+    id:6,
+    title: "Bandolera",
+    price: 2700,
+    image: 'producto6.jpeg',
+    stock: 5,
+    image2: 'producto6.jpeg',
+    image3: 'producto6.jpeg',
+    description: "Bandolera Reversible",
+    category: "bolsosybandoleras"
 }]
 
 
